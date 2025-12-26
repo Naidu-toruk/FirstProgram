089047dd0a58b113a0fff0c472431b87d61d5167
Naidu-toruk <sadinenibrahmanaidu@gmail.com>
2025-12-26 08:29:54 +0530

    updated logic

--- FirstProgram/Program.cs
+++ FirstProgram/Program.cs
@@ -11,6 +11,7 @@ namespace FirstProgram
         static void Main(string[] args)
         {
             Console.WriteLine("This is my first C# Program");
+            Console.ReadLine();
         }
     }
 }
